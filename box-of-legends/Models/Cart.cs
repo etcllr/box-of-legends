fe424c9b1f6b4aa8fb88029e9476d75d3483f95a
leo-exee <leopold@littlebill.io>
2025-01-24 11:10:16 +0100

    feat: add status column to Cart model and migration

--- box-of-legends/Models/Cart.cs
+++ box-of-legends/Models/Cart.cs
@@ -7,4 +7,5 @@ public class Cart
     public int Id { get; set; }
     public virtual required ApplicationUser ApplicationUser { get; set; }
     public virtual required ICollection<CartLine> CartLines { get; set; }
+    public required string status { get; set; }
 }
